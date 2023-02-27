022ea2f2026c0d2aee056b92a2535946d547d103
felipebrenno0 <felipebrenno213@gmail.com>
2023-02-27 05:46:30 -0300

    Changes to be committed: 	new file:   css/style.css 	new file:   image/clouds.png 	new file:   image/mario.gif 	new file:   image/marioDerrota.png 	new file:   image/pipe.png 	new file:   js/script.js 	new file:   mario.html

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+
+const jump = () => {
+    mario.classList.add('jump')
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump')
+
+    }, 500)
+
+}
+
+const loop = setInterval(() => { 
+
+
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = window.getComputedStyle(mario).bottom.replace('px', '')
+    
+    console.log(marioPosition)
+    if (pipePosition <= 70 && pipePosition > 0 && marioPosition <80) {
+
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './image/marioDerrota.png'
+        mario.style.width = '55px'
+        mario.style.marginLeft = '21px'
+
+        clearInterval(loop)
+
+        
+
+
+    }
+
+}, 10)
+
+document.addEventListener('keydown', jump)
